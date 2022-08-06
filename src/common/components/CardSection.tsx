d27b0b625f90a9579ed39d36dd973e2260e94b5e
Johan <grimmerw968@gmail.com>
2022-08-06 12:18:49 +0300

    feat: import store, loading state, add userByDate func

--- src/common/components/CardSection.tsx
+++ src/common/components/CardSection.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
 import { Card } from 'common/components/Card';
+import { getUserStore, userController } from 'store/getUser/userStore';
+import { LoadStatus } from 'common/const/load-status.const';
 
-export const CardSection = () => {
+export const CardSection = observer(() => {
+  const [pageNum, setPageNum] = useState<number>(1);
+
+  const { loading, usersByDate, store } = getUserStore;
+  const { total_pages } = store;
+
+  useEffect(() => {
+    userController.uploadUsers(pageNum);
+  }, [pageNum]);
+
+  if (loading === LoadStatus.pending) {
+    return <div />;
+  }
+
   return (
     <section>
       <div className="cards__inner">
@@ -9,10 +26,20 @@ export const CardSection = () => {
           <div className="cards__heading__wrapper">
             <h2 className="cards__heading">Working with GET request</h2>
           </div>
-          <Card />
-          <div className="cards__show">Show more</div>
+          <div className="cards__items">
+            {usersByDate.map((item) => (
+              <Card key={item.id} {...item} />
+            ))}
+          </div>
+          <div className="cards__btn__inner">
+            {pageNum === total_pages ? null : (
+              <button className="cards__btn__show" onClick={() => setPageNum((prev) => prev + 1)}>
+                Show more
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </section>
   );
-};
+});
