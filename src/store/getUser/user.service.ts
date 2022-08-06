c18f583dc14a2aebbb04b8296c9d0daa211c614a
Johan <grimmerw968@gmail.com>
2022-08-06 12:16:49 +0300

    feat: add load service and pagination in user.service

--- src/store/getUser/user.service.ts
+++ src/store/getUser/user.service.ts
@@ -1,5 +1,16 @@
+/* eslint-disable no-useless-constructor */
 import { UserStore } from 'store/getUser/userStore';
+import { TLoad } from 'store/getPosition/interface';
+import { IUserResponse, IUserService } from 'store/getUser/interface';
 
-export class UserService {
+export class UserService implements IUserService {
   constructor(private readonly store: UserStore) {}
+
+  loadService(load: TLoad) {
+    return this.store.loadStatus(load);
+  }
+
+  pagination(data: IUserResponse) {
+    return this.store.upload(data);
+  }
 }
