b9d1e2000412e02c0e8636ec809a4ff12467edc4
Johan <grimmerw968@gmail.com>
2022-08-06 12:17:21 +0300

    feat: add interface to store getUser

--- src/store/getUser/interface.ts
+++ src/store/getUser/interface.ts
@@ -1,3 +1,51 @@
-export interface IUserStore {}
-export interface IUserController {}
-export interface IUserService {}
+import { TLoad } from 'store/getPosition/interface';
+
+export interface ILinks {
+  next_url: null | string;
+  prev_url: null | string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  photo: string;
+  position: string;
+  position_id: number;
+  registration_timestamp: number;
+}
+
+export interface IUserResponse {
+  success: boolean;
+  total_pages: number;
+  total_users: number;
+  count: number;
+  links: ILinks;
+  users: Array<IUser>;
+  page: number;
+}
+
+type TUserWithNull<T> = {
+  [P in keyof T]: T[P] | null;
+};
+
+export type TUserState = TUserWithNull<IUserResponse>;
+
+export interface IUserStore {
+  loading: TLoad;
+  isStarted: boolean;
+  store: TUserState;
+  loadStatus(load: TLoad): void;
+  upload(data: IUserResponse): void;
+  get usersByDate(): Array<IUser>;
+}
+
+export interface IUserController {
+  uploadUsers(page: number): void;
+}
+
+export interface IUserService {
+  loadService(load: TLoad): void;
+  pagination(data: IUserResponse): void;
+}
