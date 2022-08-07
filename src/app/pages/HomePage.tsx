4c4bde46f319dead9391d471f945337961ee3d92
Johan <grimmerw968@gmail.com>
2022-08-07 19:47:54 +0300

    feat: add container for main section and layout

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Header } from 'common/components/Header';
+import { HeaderRectangle } from 'common/components/HeaderRectangle';
+import { MainSection } from 'common/components/MainSection';
+import { Layout } from 'app/pages/Layout';
 
 export const HomePage = () => {
-  return <div />;
+  return (
+    <>
+      <HeaderRectangle />
+      <Header />
+      <div className="container">
+        <MainSection />
+        <Layout />
+      </div>
+    </>
+  );
 };
