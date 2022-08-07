7d3821fabb03dbaccdff0a109c5963bccdd6621f
Johan <grimmerw968@gmail.com>
2022-08-07 19:45:46 +0300

    feat: add anchor for a tag

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -7,8 +7,12 @@ export const Header = () => {
         <div className="header__wrapper">
           <div className="header__logo" />
           <div className="header__items">
-            <div className="header__btn-user">Users</div>
-            <div className="header__btn-sign-up">Sign Up</div>
+            <a href="#users" className="header__btn-user">
+              Users
+            </a>
+            <a href="#sign-in" className="header__btn-sign-up">
+              Sign Up
+            </a>
           </div>
         </div>
       </div>
