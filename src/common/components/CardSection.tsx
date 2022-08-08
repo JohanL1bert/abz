7809215d90fe437bed9f11fcda068cc8d1e7cb5f
Johan <grimmerw968@gmail.com>
2022-08-08 14:57:51 +0300

    feat: add preloader to card section

--- src/common/components/CardSection.tsx
+++ src/common/components/CardSection.tsx
@@ -4,27 +4,26 @@ import { observer } from 'mobx-react-lite';
 import { Card } from 'common/components/Card';
 import { getUserStore, userController } from 'store/getUser/userStore';
 import { LoadStatus } from 'common/const/load-status.const';
+import { Preloader } from 'common/components/Preloader';
 
 export const CardSection = observer(() => {
   const [pageNum, setPageNum] = useState<number>(1);
 
-  const { loading, usersByDate, store } = getUserStore;
+  const { loading, usersByDate, store, isAuth } = getUserStore;
   const { total_pages } = store;
 
   useEffect(() => {
     userController.uploadUsers(pageNum);
-  }, [pageNum]);
-
-  if (loading === LoadStatus.pending) {
-    return <div />;
-  }
+  }, [pageNum, isAuth]);
 
   return (
     <section>
       <div className="cards__inner">
         <div className="cards__wrapper">
           <div className="cards__heading__wrapper">
-            <h2 className="cards__heading">Working with GET request</h2>
+            <h2 className="cards__heading" id="users">
+              Working with GET request
+            </h2>
           </div>
           <div className="cards__items">
             {usersByDate.map((item) => (
@@ -32,7 +31,9 @@ export const CardSection = observer(() => {
             ))}
           </div>
           <div className="cards__btn__inner">
-            {pageNum === total_pages ? null : (
+            {loading === LoadStatus.pending ? (
+              <Preloader />
+            ) : pageNum === total_pages ? null : (
               <button className="cards__btn__show" onClick={() => setPageNum((prev) => prev + 1)}>
                 Show more
               </button>
