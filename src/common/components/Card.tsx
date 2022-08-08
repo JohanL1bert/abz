fc8a9d4936f17e711b18ccbf5559be3ec16a715a
Johan <grimmerw968@gmail.com>
2022-08-08 14:53:02 +0300

    feat: add custom tooltip

--- src/common/components/Card.tsx
+++ src/common/components/Card.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { IUser } from 'store/getUser/interface';
+import { CustomTooltip } from 'common/components/CustomTooltip';
 
-export const Card = () => {
+export const Card = (props: IUser) => {
+  const { name, email, phone, photo, position } = props;
   return (
     <div className="card__wrapper">
-      <img src="" alt="" className="card__img" />
-      <p className="card__name">1</p>
-      <p className="card__position">2</p>
-      <a href="malito:" className="card__email">
-        email
-      </a>
-      <div className="card__phone">232</div>
+      <img src={photo} alt="" className="card__img" />
+      <p className="card__name">{name}</p>
+      <div className="card__inner">
+        <p className="card__position">{position}</p>
+
+        <CustomTooltip title={email}>
+          <a href={`malito:${email}`} className="card__email">
+            {email}
+          </a>
+        </CustomTooltip>
+
+        <a href={`tel:${phone}`} className="card__phone">
+          {phone}
+        </a>
+      </div>
     </div>
   );
 };
