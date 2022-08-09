e87536395e761e4b8e1d290948b036c127f940fd
Johan <grimmerw968@gmail.com>
2022-08-09 11:49:53 +0300

    feat: add post api

--- src/common/http.service/api.ts
+++ src/common/http.service/api.ts
@@ -4,6 +4,10 @@ const apiService = {
   get(url: string, config?: any) {
     return axios.get(url, config);
   },
+
+  post(url: string, data: any, config: any) {
+    return axios.post(url, data, config);
+  },
 };
 
 export { apiService };
